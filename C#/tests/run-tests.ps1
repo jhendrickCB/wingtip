5e2f4aaa9d235b05b4edfc4deec067402255da52
Josh Hendrick <jhendrick@gmail.com>
2018-06-29 10:58:39 -0700

    reverted

--- C#/tests/run-tests.ps1
+++ C#/tests/run-tests.ps1
@@ -1,4 +1,4 @@
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\selenium\geckodriver-v0.21.0-win64", "Process")
+$env:path = "$env:path;C:\selenium\geckodriver-v0.21.0-win64"
 
 C:\"Program Files\nodejs\npm" install
 C:\"Program Files\nodejs\node.exe" selenium-test.js
