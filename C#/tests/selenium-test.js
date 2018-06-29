c7526a1a864da3037e2b7cb8d1c085adb075d430
Josh Hendrick <jhendrick@gmail.com>
2018-06-28 19:53:03 -0700

    Added node.js tests

---
+++ C#/tests/selenium-test.js
@@ -0,0 +1,11 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://www.google.com');
+
+driver.findElement(By.name('q')).sendKeys('cloudbees');
