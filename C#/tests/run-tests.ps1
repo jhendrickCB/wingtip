3c2fccefa1024b795e329b1a52282f21d263b942
Josh Hendrick <jhendrick@gmail.com>
2018-06-29 09:48:22 -0700

    Removed webdriver exe and added ;

--- C#/tests/run-tests.ps1
+++ C#/tests/run-tests.ps1
@@ -1,4 +1,4 @@
-$env:path += "C:\selenium\geckodriver-v0.21.0-win64"
+$env:path += ";C:\selenium\geckodriver-v0.21.0-win64"
 
 C:\"Program Files\nodejs\npm" install
 C:\"Program Files\nodejs\node.exe" selenium-test.js
