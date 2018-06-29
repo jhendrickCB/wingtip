7a2bb9bdf7423cf715b5ab65241388b87e30a641
Josh Hendrick <jhendrick@gmail.com>
2018-06-29 10:55:42 -0700

    New method for setting path

--- C#/tests/run-tests.ps1
+++ C#/tests/run-tests.ps1
@@ -1,4 +1,4 @@
-$env:path = "$env:path;C:\selenium\geckodriver-v0.21.0-win64"
+[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\selenium\geckodriver-v0.21.0-win64", "Machine")
 
 C:\"Program Files\nodejs\npm" install
 C:\"Program Files\nodejs\node.exe" selenium-test.js
