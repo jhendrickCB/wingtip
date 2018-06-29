c0bbe11d94bc19b90bc05039e01db6cbd63609bc
Josh Hendrick <jhendrick@gmail.com>
2018-06-29 11:22:07 -0700

    Quick test

--- C#/tests/run-tests.ps1
+++ C#/tests/run-tests.ps1
@@ -1,4 +1,4 @@
-$env:path = "$env:path;C:\selenium\geckodriver-v0.21.0-win64"
+$env:path = "$env:path;C:\selenium\geckodriver-v0.21.0-win64;C:\Program Files\nodejs"
 
-C:\"Program Files\nodejs\npm" install
-C:\"Program Files\nodejs\node.exe" selenium-test.js
+npm install
+node selenium-test.js
